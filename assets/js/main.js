4abe83abbdeb00175286f576cb6cc45ba1ba2691
gabrielosorioz <gabrielosorio013@gmail.com>
2024-02-06 14:58:40 -0300

    feat(home): :sparkles: Initialize Home Swiper / mplement scroll header
    Configures a Swiper instance for ".home-swiper" with specified settings.  Adds/removes 'scroll-header' class based on scroll position

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,10 +32,26 @@
 
 
 /*=============== HOME SWIPER ===============*/
+var Homeswiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    Loop:'true',
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollheader() {
+  const header = document.getElementById('header')
+  // when the scroll is greater than 50 viewprot height, add the scroll-header class tot he header
+  if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
 
+window.addEventListener('scroll', scrollheader) 
 
 /*=============== NEW SWIPER ===============*/
 
